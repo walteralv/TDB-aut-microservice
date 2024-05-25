f213bbf4676740bfa9c947cb547029966ca1f3c2
Walter Alvarado <walvdev@gmail.com>
2024-05-25 10:14:11 -0500

    resolve docker mongo problems

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  
   NATS_SERVERS: string[];
-
   JWT_SECRET: string;
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
-  
   NATS_SERVERS: joi.array().items( joi.string() ).required(),
   JWT_SECRET: joi.string().required(),
 })
@@ -33,8 +30,6 @@ const envVars:EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-
   natsServers: envVars.NATS_SERVERS,
-
   jwtSecret: envVars.JWT_SECRET,
 };
